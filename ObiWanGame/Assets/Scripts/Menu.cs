7b96fc63c3c7a1a2ef60a0b46c92febb634afa0e
Lewis Hamilton <ham-53@hotmail.co.uk>
2019-05-27 02:23:42 +0100

    Added Menu

---
+++ ObiWanGame/Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void Play()
+    {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene("Fight");
+    }
+
+}
