7ba176b73ae831bece20fe61ef3d25b1c05b1ad1
Lewis Hamilton <ham-53@hotmail.co.uk>
2019-07-07 12:13:05 +0100

    Added a timer that will be used tfor a leaderboard

---
+++ ObiWanGame/Assets/Scripts/timer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+    private int time = 0;
+
+
+    //components
+    private Text timeText;
+    private FightState fs;
+
+    // Use this for initialization
+    void Start () {
+        fs = GameObject.Find("Countdown").GetComponent<FightState>();
+        timeText = GetComponent<Text>();
+
+        InvokeRepeating("counter", 0, 1.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+
+	}
+
+    void counter()
+    {
+
+        //start counter when fight starts
+
+        if (fs.fight )
+        {
+            time += 1;
+
+            timeText.text = time.ToString();
+
+            if (timeText.text.Length == 1)
+            {
+                timeText.text = "00" + timeText.text;
+            }
+
+
+            if (timeText.text.Length == 2)
+            {
+                timeText.text = "0" + timeText.text;
+            }
+
+            if(timeText.text.Length > 3)
+            {
+                timeText.text = "999";
+            }
+        }
+       
+
+    }
+
+}
